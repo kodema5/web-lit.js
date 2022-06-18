49de6574d42779c34efbb4f111a73a7b20d16814
kodema5 <kodema5@outlook.com>
2022-06-17 17:36:11 -0700

    add modules

--- src/index.js
+++ src/index.js
@@ -2,7 +2,25 @@
 // deno bundle -r src/_mod.js src/mod.js
 // npx esbuild --bundle src/index.js --sourcemap --outfile=web-lit.js --format=esm --minify --watch
 
-export * from './mod.js'
+
+import * as Ajax from '../ajax.js/mod.js'
+export { pubsub, PubSub }  from '../pubsub.js/mod.js'
+export * as worker from '../waaf.js/mod.js'
+import { Store } from '../store.js/mod.js'
+
+// a local store
+let store = new Store('web-lit')
+store.load()
+
+// init ajax-headers
+let AJAX_AUTH_ID = 'ajax.headers.Authorization'
+{    let a = store.get(AJAX_AUTH_ID, null)
+    if (a) {
+        Ajax.ajax.headers.Authorization = a
+    }
+}
+
+export { Ajax, store, Store }
 
 export * from './styles.js'
 
